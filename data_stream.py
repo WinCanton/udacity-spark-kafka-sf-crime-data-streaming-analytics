011aeb30ec76ee08c5b1e323404d6c341c7f92bd
martin simanjuntak <martinms@gmail.com>
2021-09-23 13:58:38 +0100

    Update data_stream.py

--- data_stream.py
+++ data_stream.py
@@ -5,7 +5,7 @@ from pyspark.sql.types import *
 import pyspark.sql.functions as psf
 
 
-# TODO Create a schema for incoming resources
+# Create a schema for incoming resources
 schema = StructType([
             StructField("address", StringType(), True),
             StructField("address_type", StringType(), True),
@@ -25,7 +25,7 @@ schema = StructType([
 
 def run_spark_job(spark):
 
-    # TODO Create Spark Configuration
+    # Create Spark Configuration
     # Create Spark configurations with max offset of 200 per trigger
     # set up correct bootstrap server and port
     df = spark \
@@ -41,7 +41,7 @@ def run_spark_job(spark):
     # Show schema for the incoming resources for checks
     df.printSchema()
 
-    # TODO extract the correct column from the kafka input resources
+    # extract the correct column from the kafka input resources
     # Take only value and convert it to String
     kafka_df = df.selectExpr("CAST(value AS STRING)")
 
@@ -49,14 +49,14 @@ def run_spark_job(spark):
         .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
         .select("DF.*")
 
-    # TODO select original_crime_type_name and disposition
+    # select original_crime_type_name and disposition
     distinct_table = service_table.select(["original_crime_type_name", "disposition"])
 
     # count the number of original crime type
     agg_df = distinct_table.groupBy(distinct_table.original_crime_type_name).count().orderBy('count',ascending=False)
 
-    # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
-    # TODO write output stream
+    # Q1. Submit a screen shot of a batch ingestion of the aggregation
+    # write output stream
     query = agg_df \
         .writeStream \
         .trigger(processingTime="10 seconds") \
@@ -66,20 +66,20 @@ def run_spark_job(spark):
         .start()
 
 
-    # TODO attach a ProgressReporter
+    # attach a ProgressReporter
     query.awaitTermination()
 
-    # TODO get the right radio code json path
+    # get the right radio code json path
     radio_code_json_filepath = "./radio_code.json"
     radio_code_df = spark.read.option("multiLine", True).json(radio_code_json_filepath)
 
     # clean up your data so that the column names match on radio_code_df and agg_df
     # we will want to join on the disposition code
 
-    # TODO rename disposition_code column to disposition
+    # rename disposition_code column to disposition
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
-    # TODO join on disposition column
+    # join on disposition column
     join_query = agg_df \
         .join(radio_code_df, "disposition") \
         .writeStream \
@@ -94,7 +94,7 @@ def run_spark_job(spark):
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
 
-    # TODO Create Spark in Standalone mode
+    # Create Spark in Standalone mode
     spark = SparkSession \
         .builder \
         .master("local[*]") \
