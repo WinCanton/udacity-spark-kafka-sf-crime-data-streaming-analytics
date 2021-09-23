315d30539f47d0834962385861e8e7dcb020e996
martin simanjuntak <martinms@gmail.com>
2021-09-23 13:56:26 +0100

    Update kafka_server.py

--- kafka_server.py
+++ kafka_server.py
@@ -2,10 +2,10 @@ import producer_server
 
 
 def run_kafka_server():
-	# TODO get the json file path
+	# get the json file path
     input_file = "./police-department-calls-for-service.json"
 
-    # TODO fill in blanks
+    # fill in blanks
     producer = producer_server.ProducerServer(
         input_file=input_file,
         topic="udacity.project.sfcrime.analytics",
